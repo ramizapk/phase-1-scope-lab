c9b7a3e397e594e1e9e8ea72e086108079e9ef5a
Ramiz Ali <ramiz.apk7@gmail.com>
2023-01-03 05:00:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer='ramiz';
+
+function upperCaseCustomerName() {
+     customerName = customerName.toUpperCase();
+}
+
+
+
+function setBestCustomer() {
+     bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newName) {
+    bestCustomer = newName;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer='ali';
+}
